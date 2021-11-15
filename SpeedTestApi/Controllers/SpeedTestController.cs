4a097a7e6a6fb84d75b60ab798801b7fb6dc8330
haako <haako@computas.com>
2021-11-15 18:13:12 +0100

    added uploadspeedtest endpoint

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SpeedTestApi.Models;
 
 namespace SpeedTestApi.Controllers
 {
@@ -6,12 +8,26 @@ namespace SpeedTestApi.Controllers
     [ApiController]
     public class SpeedTestController : ControllerBase
     {
+        private readonly ILogger _logger;
+
+        public SpeedTestController(ILogger<SpeedTestController> logger)
+        {
+            _logger = logger;
+        }
         // GET speedtest/ping
         [Route("ping")]
         [HttpGet]
         public string Ping()
         {
             return "PONG";
+        }
+        [HttpPost]
+        public string UploadSpeedTest([FromBody] TestResult speedTest)
+        {
+            var response = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+            _logger.LogInformation(response);
+
+            return response;
         }
     }
 }
